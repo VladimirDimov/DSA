4f7cfb345181f8466955a5d8d2e6f24ff17fe1cd
VladimirDimov <vdimov1124@gmail.com>
2015-11-04 23:56:44 +0200

    Recursion some tasks added

---
+++ Homework/Recursion/CombinationsWithoutDuplicates/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CombinationsWithDuplicates
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the length of the sequence.");
+            var n = int.Parse(Console.ReadLine());
+            var combinations = new List<List<int>>();
+            GenerateAllCombinationsWithDuplicates(n, 1, new List<int>(), combinations);
+
+            PrintCollection(combinations);
+        }
+
+        private static void GenerateAllCombinationsWithDuplicates(int n, int left, List<int> combination, List<List<int>> result)
+        {
+            if (combination.Count == n)
+            {
+                result.Add(combination);
+                return;
+            }
+
+            for (int i = left; i < n + 1; i++)
+            {
+                var currentCombination = new List<int>(combination);
+                currentCombination.Add(i);
+                GenerateAllCombinationsWithDuplicates(n, i, currentCombination, result);
+            }
+        }
+
+        private static void PrintCollection(List<List<int>> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+    }
+}
---
+++ Homework/Recursion/Permutations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Permutations
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the length of the sequence to permute:");
+            var n = int.Parse(Console.ReadLine());
+
+            var result = new List<List<int>>();
+            Permute(n, new List<int>(), new HashSet<int>(), result);
+
+            PrintCollection(result);
+        }
+
+        private static void Permute(int n, List<int> permutation, HashSet<int> passed, List<List<int>> result)
+        {
+            if (permutation.Count == n)
+            {
+                result.Add(permutation);
+            }
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                if (!passed.Contains(i))
+                {
+                    var currentPermutation = new List<int>(permutation);
+                    var currentPassed = new HashSet<int>(passed);
+                    currentPermutation.Add(i);
+                    currentPassed.Add(i);
+                    Permute(n, currentPermutation, currentPassed, result);
+                }
+            }
+        }
+
+        private static void PrintCollection(List<List<int>> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+    }
+}
