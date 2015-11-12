f23c016f0d05cfa88184640390230d0c73f3484e
VladimirDimov <vdimov1124@gmail.com>
2015-11-12 10:27:05 +0200

    Combinatorics hw

---
+++ Homework/09.Combinatorics/BinaryPasswords/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Combinatorics
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine((long)Math.Pow(2, Console.ReadLine().Where(ch => ch == '*').Count()));
+        }
+    }
+}
---
+++ Homework/09.Combinatorics/Rabbits/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ColorRabbits
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var numberOfRabbits = int.Parse(Console.ReadLine());
+            var rabbitsDiction = new Dictionary<ulong, ulong>();
+
+            for (int i = 0; i < numberOfRabbits; i++)
+            {
+                var currentAnswer = ulong.Parse(Console.ReadLine());
+                if (rabbitsDiction.ContainsKey(currentAnswer))
+                {
+                    rabbitsDiction[currentAnswer]++;
+                }
+                else
+                {
+                    rabbitsDiction[currentAnswer] = 1;
+                }
+            }
+
+            ulong counter = 0;
+            foreach (var item in rabbitsDiction)
+            {
+                ulong answer = item.Key;
+                ulong numberOfAsked = item.Value;
+
+                counter += (ulong)Math.Ceiling((decimal)numberOfAsked / (answer + 1)) * (answer + 1);
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
