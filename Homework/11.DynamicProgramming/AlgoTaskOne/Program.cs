c007cb3503b66b9b0a7f2e1cd4e20cd417e3b6de
VladimirDimov <vdimov1124@gmail.com>
2015-11-25 15:26:51 +0200

    added algo task 2

---
+++ Homework/11.DynamicProgramming/AlgoTaskOne/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AlgoTaskOne
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            long[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => long.Parse(x))
+                .ToArray();
+
+            var a = input[0];
+            var b = input[1];
+            var c = input[2];
+            var n = input[3];
+
+            Console.WriteLine(Tribonacci(a, b, c, n));
+        }
+
+        static long Tribonacci(long a, long b, long c, long n)
+        {
+            if (n == 1)
+            {
+                return a;
+            }
+            else if (n == 2)
+            {
+                return b;
+            }
+            else if (n == 3)
+            {
+                return c;
+            }
+
+            long counter = 3;
+            long result = 0;
+            while (counter < n)
+            {
+                result = a + b + c;
+                a = b;
+                b = c;
+                c = result;
+                counter++;
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ Homework/11.DynamicProgramming/AlgoTaskTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SuperSum
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        private static int sum;
+
+        public static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var k = inputLine[0];
+            var n = inputLine[1];
+            sum = 0;
+
+            SuperSum(k, n);
+            Console.WriteLine(sum);
+        }
+
+        private static void SuperSum(int k, int n)
+        {
+            if (k == 0)
+            {
+                sum += n;
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                SuperSum(k - 1, i);
+            }
+        }
+    }
+}
